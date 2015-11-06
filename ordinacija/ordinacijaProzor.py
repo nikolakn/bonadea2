2a6f2479ee2cf2cfd03e28f8b83dd87dd8875f63
nikolakn <nkcodeplus@gmail.com>
2015-11-06 01:40:07 +0100

    init2

---
+++ ordinacija/ordinacijaProzor.py
@@ -0,0 +1,20 @@
+'''
+Created on Nov 6, 2015
+
+@author: nikola
+'''
+
+#import mysql.connector
+from PyQt4.QtGui import * # @UnusedWildImport
+from PyQt4.QtCore import * # @UnusedWildImport
+
+class ordinacijaProzor(QMainWindow):
+    
+    def __init__(self,login,name):
+        super(ordinacijaProzor, self).__init__()
+        self.initUI(name)
+        
+    def initUI(self, nn):
+        self.setWindowState(Qt.WindowMaximized)
+        self.setWindowTitle("Bonadea - "+nn)
+        self.show()
