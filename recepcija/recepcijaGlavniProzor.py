26a75c4527e10b39fca1caf311a1e13a188eeb16
nikolakn <nkcodeplus@gmail.com>
2015-11-27 05:21:58 +0100

    rec

--- recepcija/recepcijaGlavniProzor.py
+++ recepcija/recepcijaGlavniProzor.py
@@ -13,6 +13,32 @@ class recepcijaProzor(QMainWindow):
     def __init__(self,login,name):
         super(recepcijaProzor, self).__init__()
         self.initUI()
+        
+        menubar = self.menuBar()
+        
+        novipacijentAction = QAction('&Novi pacijent', self)        
+        novipacijentAction.setShortcut('Ctrl+N')
+        #novipacijentAction.triggered.connect(QtGui.qApp.quit)
+        pretragapacijentAction = QAction('&Pretraga', self)        
+        pretragapacijentAction.setShortcut('Ctrl+P')       
+        obrisipacijentAction = QAction('&Obriši pacijenta', self)        
+        obrisipacijentAction.setShortcut('Ctrl+O')
+        
+        izmeniosobljeAction = QAction('&izmeni podatke', self)        
+        izmeniosobljeAction.setShortcut('Ctrl+i')       
+        dodajosobljeAction = QAction('&Dodaj osoblje', self)        
+        dodajosobljeAction.setShortcut('Ctrl+d')
+        
+        pacijentiMenu = menubar.addMenu('&Pacijenti')
+        pacijentiMenu.addAction(novipacijentAction)
+        pacijentiMenu.addAction(pretragapacijentAction)
+        pacijentiMenu.addAction(obrisipacijentAction)
+        osobljeMenu = menubar.addMenu('&Osoblje')
+        osobljeMenu.addAction(izmeniosobljeAction)
+        osobljeMenu.addAction(dodajosobljeAction)
+        #fileMenu.addAction(exitAction)
+        
+        
         self.window = QWidget();
         gornji = QHBoxLayout()
         cal = QCalendarWidget(self)
@@ -24,17 +50,64 @@ class recepcijaProzor(QMainWindow):
         gornji.addWidget(cal)
         gornji.addWidget(lista)
           
+          
+          
         srednji =  QHBoxLayout()
-        button3 =  QPushButton("Three");
-        button4 =  QPushButton("Four");
-        srednji.addWidget(button3)
-        srednji.addWidget(button4)
+        
+        srednjilevi = QVBoxLayout()
+        
+        srednjivreme =  QHBoxLayout()
+        label    = QLabel("Vreme: "+QTime.currentTime().toString("hh:mm"))
+        f = QFont( "Arial", 10, QFont.Bold);
+        label.setFont(f);
+        label.setFixedWidth(250)
+        button3 =  QPushButton("Zakazati");
+        button4 =  QPushButton("Poništi zakazano");
+        srednjivreme.addWidget(label)
+        srednjivreme.addWidget(button3)
+        srednjivreme.addWidget(button4)
+        
+        srednjiime =  QHBoxLayout()
+        labelime    = QLabel("Prezime i ime:")
+        labelime.setFont(f);
+        self.comboime = QComboBox()
+        self.comboime.setEditable(True)
+        self.comboime.setFixedWidth(150)
+        button5 =  QPushButton("Novi");
+        button6 =  QPushButton("Obriši");
+        srednjiime.addWidget(labelime)
+        srednjiime.addWidget(self.comboime)
+        srednjiime.addWidget(button5)
+        srednjiime.addWidget(button6)
+    
+        srednjilevi.addLayout(srednjivreme)
+        srednjilevi.addLayout(srednjiime)
+        
+        srednjidesni =  QHBoxLayout()
+        listazavrsenih =  QListWidget(self) 
+        listazavrsenih.setFixedHeight(100)
+        button7 =  QPushButton("Očisti");
+        srednjidesni.addWidget(listazavrsenih)
+        srednjidesni.addWidget(button7)
+        
+        srednji.addLayout(srednjilevi)
+        srednji.addStretch()
+        srednji.addLayout(srednjidesni)
         srednji.setSizeConstraint(QLayout.SetMaximumSize)
                 
         donji =   QHBoxLayout()
+        donji.SetNoConstraint = True
         tab =  QTabWidget(self)
         tab1 = QWidget();
         tab2 = QWidget();
+        donjitab1levo =   QHBoxLayout()
+        listaposete =  QListWidget(self) 
+        listaposete.setFixedWidth(150)
+        listanalaz =  QTextEdit(self) 
+        donjitab1levo.addWidget(listaposete)
+        donjitab1levo.addWidget(listanalaz)
+        tab2.setLayout(donjitab1levo)
+        
         tab.addTab(tab1, "O pacijentu")
         tab.addTab(tab2, "posete ordiniciji")
         donji.setSizeConstraint(QLayout.SetMaximumSize)
@@ -45,7 +118,7 @@ class recepcijaProzor(QMainWindow):
         layout.addLayout(gornji)
         layout.addLayout(srednji)
         layout.addLayout(donji)
-        layout.addStretch()
+        #layout.addStretch()
         
         self.window.setLayout(layout)
         self.setCentralWidget(self.window);
