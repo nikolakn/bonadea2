5dba0fee8f2a8b141e2a8c58b4a3db881d5f751e
nikolakn <nkcodeplus@gmail.com>
2015-11-27 06:31:43 +0100

    recui

--- recepcija/recepcijaGlavniProzor.py
+++ recepcija/recepcijaGlavniProzor.py
@@ -28,6 +28,7 @@ class recepcijaProzor(QMainWindow):
         izmeniosobljeAction.setShortcut('Ctrl+i')       
         dodajosobljeAction = QAction('&Dodaj osoblje', self)        
         dodajosobljeAction.setShortcut('Ctrl+d')
+        opecijestampajzaglavlje = QAction('&zaglavlje', self)        
         
         pacijentiMenu = menubar.addMenu('&Pacijenti')
         pacijentiMenu.addAction(novipacijentAction)
@@ -36,6 +37,8 @@ class recepcijaProzor(QMainWindow):
         osobljeMenu = menubar.addMenu('&Osoblje')
         osobljeMenu.addAction(izmeniosobljeAction)
         osobljeMenu.addAction(dodajosobljeAction)
+        opcije = menubar.addMenu('Opcije')
+        opcije.addAction(opecijestampajzaglavlje)
         #fileMenu.addAction(exitAction)
         
         
@@ -51,7 +54,8 @@ class recepcijaProzor(QMainWindow):
         gornji.addWidget(lista)
           
           
-          
+        srednjiwidget = QWidget(); 
+
         srednji =  QHBoxLayout()
         
         srednjilevi = QVBoxLayout()
@@ -94,29 +98,133 @@ class recepcijaProzor(QMainWindow):
         srednji.addStretch()
         srednji.addLayout(srednjidesni)
         srednji.setSizeConstraint(QLayout.SetMaximumSize)
+        srednjiwidget.setLayout(srednji)
                 
         donji =   QHBoxLayout()
         donji.SetNoConstraint = True
         tab =  QTabWidget(self)
         tab1 = QWidget();
         tab2 = QWidget();
-        donjitab1levo =   QHBoxLayout()
+        tab1layout = QVBoxLayout()
+        donjitab1gore =   QHBoxLayout()
         listaposete =  QListWidget(self) 
         listaposete.setFixedWidth(150)
         listanalaz =  QTextEdit(self) 
-        donjitab1levo.addWidget(listaposete)
-        donjitab1levo.addWidget(listanalaz)
-        tab2.setLayout(donjitab1levo)
+        donjitab1gore.addWidget(listaposete)
+        donjitab1gore.addWidget(listanalaz)
         
+        donjitab1dole =   QHBoxLayout()
+        labevrsta   = QLabel("Vrsta intervencije:")
+        labedoktor   = QLabel("Doktor:")
+        intervencija = QLineEdit(self)
+        doktor = QLineEdit(self)
+        stampaj =  QPushButton("Štampaj nalaz");
+        donjitab1dole.addWidget(labevrsta)
+        donjitab1dole.addWidget(intervencija)
+        donjitab1dole.addWidget(labedoktor)
+        donjitab1dole.addWidget(doktor)
+        donjitab1dole.addWidget(stampaj)
+        
+        tab1layout.addLayout(donjitab1gore)
+        tab1layout.addLayout(donjitab1dole)
+        tab2.setLayout(tab1layout)
+        
+        tablayout = QVBoxLayout()
+        tgornji =   QHBoxLayout()
+        
+        brojkartona = QVBoxLayout()
+        brojkartonalab   = QLabel("Broj Kartona:")
+        brojkartonaed = QLineEdit(self)
+        brojkartonaed.setMaximumWidth(50)
+        brojkartona.addWidget(brojkartonalab)
+        brojkartona.addWidget(brojkartonaed)
+ 
+        t1ime = QVBoxLayout()
+        t1imelab   = QLabel("Prezime i ime:")
+        t1imeaed = QLineEdit(self)
+        t1imeaed.setMaximumWidth(250)
+        t1ime.addWidget(t1imelab)
+        t1ime.addWidget(t1imeaed)
+        
+        t1datum= QVBoxLayout()
+        t1datumlab   = QLabel("Datum rodjenja:")
+        t1datumed = QDateTimeEdit (self)
+        t1datumed.setDisplayFormat("dd.MM.yyyy");
+        t1datumed.setCalendarPopup(True)
+        t1datumed.setMaximumWidth(150)
+        t1datum.addWidget(t1datumlab)
+        t1datum.addWidget(t1datumed)
+        
+        t1pol = QVBoxLayout()
+        t1pollab   = QLabel("Pol:")
+        t1poled = QComboBox(self)
+        t1pol.addWidget(t1pollab)
+        t1pol.addWidget(t1poled)
+
+        t1kontakt= QVBoxLayout()
+        t1kontaktlab   = QLabel("Kontakt:")
+        t1kontakted = QLineEdit(self)
+        t1kontakt.addWidget(t1kontaktlab)
+        t1kontakt.addWidget(t1kontakted)
+                  
+        
+        tgornji.addLayout(brojkartona)
+        tgornji.addLayout(t1ime)
+        tgornji.addLayout(t1datum)
+        tgornji.addLayout(t1pol)
+        tgornji.addLayout(t1kontakt)
+        
+        tdonji  =   QHBoxLayout()       
+        napomena = QVBoxLayout()
+        napomenalab   = QLabel("NAPOMENA:")
+        napomenaed = QTextEdit(self)
+        napomena.addWidget(napomenalab)
+        napomena.addWidget(napomenaed)
+        
+        alerg = QVBoxLayout()
+        alerglab   = QLabel("MEDIKAMENTOZNE ALERGIJE:")
+        alerged = QTextEdit(self)
+        alerg.addWidget(alerglab)
+        alerg.addWidget(alerged)
+        
+        dalerg = QVBoxLayout()
+        dalerglab   = QLabel("DRUGE ALERGIJE:")
+        dalerged = QTextEdit(self)
+        dalerg.addWidget(dalerglab)
+        dalerg.addWidget(dalerged)
+
+        hron = QVBoxLayout()
+        hronlab   = QLabel("HRONIČNA OBOLJENJA:")
+        hroned = QTextEdit(self)
+        hron.addWidget(hronlab)
+        hron.addWidget(hroned)
+                
+        tdonji.addLayout(napomena)   
+        tdonji.addLayout(alerg)  
+        tdonji.addLayout(dalerg)  
+        tdonji.addLayout(hron) 
+        
+        tablayout.addLayout(tgornji)
+        tablayout.addLayout(tdonji)
+        tablayout.addStretch()
+        tab1.setLayout(tablayout)
+       
         tab.addTab(tab1, "O pacijentu")
         tab.addTab(tab2, "posete ordiniciji")
         donji.setSizeConstraint(QLayout.SetMaximumSize)
+              
         donji.addWidget(tab)       
         
         
+        
+        srednjiwidget.setAutoFillBackground(True)
+        p = srednjiwidget.palette()
+        p.setColor(srednjiwidget.backgroundRole(), Qt.cyan)
+        srednjiwidget.setPalette(p)
+        
         layout = QVBoxLayout()
         layout.addLayout(gornji)
-        layout.addLayout(srednji)
+        layout.addWidget(srednjiwidget)
         layout.addLayout(donji)
         #layout.addStretch()
         
